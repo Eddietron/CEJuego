52397cd96a49bd34f27350db0ff18641d21c37e2
Eddieson <editiend@gmail.com>
2018-10-31 18:06:25 -0600

    Proyecto c#

---
+++ juegobrgas/juegobrgas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace juegobrgas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
